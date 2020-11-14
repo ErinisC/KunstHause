f3ec2ca50f1c8c0f00e1051ab40905cb62b9bf72
Vezona <vezona5tv@gmail.com>
2020-11-14 21:11:21 +0800

    add shopCart pages all

---
+++ 5_shopCart-pay.php
@@ -0,0 +1,29 @@
+<?php $title = '購物車 選擇付款方式'; ?>
+
+<?php include __DIR__ . '/1_parts/0_config.php'; ?>
+<?php include __DIR__ . '/1_parts/1_head.php'; ?>
+
+<!-- 引入ShopCart BG 的ＣＳＳ -->
+<link rel="stylesheet" href="./css/5_shopCart-bg.css">
+<!-- 引入自己購物車清單的ＣＳＳ -->
+<link rel="stylesheet" href="./css/5_shopCart-pay.css">
+
+<!-- 引入navbar -->
+<?php include __DIR__ . '/1_parts/2_navbar.php'; ?>
+<!-- 引入購物車BG -->
+<?php include __DIR__ . '/5_shopCart-bg-and-nv.php'; ?>
+
+
+<!-- 內容開始 -->
+<p>aaa</p>
+
+
+
+<!-- 購物車ＢＧ結尾 -->
+</div>
+<?php include __DIR__ . '/1_parts/3_script.php'; ?>
+
+<!-- 引入自己的ＪＳ -->
+<script src=""></script>
+
+<?php include __DIR__ . '/1_parts/4_footer.php'; ?>
