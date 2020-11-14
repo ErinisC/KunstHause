d4a29d178721203d3e3e80619d04bfb5fc1f12b3
Vezona <vezona5tv@gmail.com>
2020-11-14 13:28:33 +0800

    連接KunstHause資料庫

--- 1_parts/0_config.php
+++ 1_parts/0_config.php
@@ -1,1 +1,24 @@
 <?php define('WEB_ROOT', '/KunstHause/');
+
+
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+// 連資料庫
+$db_host = 'localhost';
+$db_name = 'KunstHause';
+$db_user = 'jin';
+$db_pass = '123123';
+
+// 這一行不可以有任何空格
+$dsn = "mysql:host={$db_host};dbname={$db_name};charset=utf8";
+
+$pdo_options = [
+    // PDO::
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+];
+
+$pdo = new PDO($dsn, $db_user, $db_pass, $pdo_options);
