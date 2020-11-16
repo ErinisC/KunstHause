36341cce699d5c6692f003fd74ec2fd7c76c5a9c
Judy Kuo <juiming0506@gmail.com>
2020-11-16 21:06:57 +0800

    -m navbar shopping cart outside

--- 1_parts/2_navbar-lg.php
+++ 1_parts/2_navbar-lg.php
@@ -14,9 +14,12 @@
         border: 3px solid black;
     }
 
+    .nav-item {
+        list-style: none;
+    }
+
     .nav-link {
         color: #000;
-        margin-right: 40px;
         font-family: "Noto Sans TC", sans-serif;
     }
 
@@ -43,10 +46,6 @@
         color: #fff;
     }
 
-    .header-icon {
-        margin-left: 2rem;
-    }
-
     .search-icon {
         top: 5px;
         right: 8px;
@@ -56,16 +55,12 @@
         cursor: pointer;
     }
 
-    .dropdown-content {
-        display: none;
-        position: absolute;
-        right: 2px;
+    .dropdown-menu {
         background-color: transparent;
-        min-width: 160px;
-        z-index: 1;
+
     }
 
-    .dropdown-content a {
+    .dropdown-item {
         float: none;
         color: black;
         background-color: #FFC024;
@@ -77,16 +72,26 @@
         text-align: left;
     }
 
-    .dropdown-content a:hover {
+    .dropdown-item:hover {
         background-color: #168FA4;
         color: #fff;
     }
 
-    .dropdown:hover .dropdown-content {
+    .dropdown:hover .dropdown-item {
         display: block;
     }
 
     /* ----------media query---------- */
+    @media (min-width: 993px) {
+        .nav-link {
+            margin-right: 40px;
+        }
+
+        .lg-none{
+            display: none;
+        }
+    }
+
     @media (max-width: 992px) {
         .header-search {
             display: none;
@@ -100,10 +105,14 @@
             display: block;
         }
 
-        .header-icon img {
-            margin-right: 2rem;
+        .member {
+            width: 50px;
+            height: 50px;
         }
 
+        .sm-none{
+            display: none;
+        }
     }
 </style>
 
@@ -111,7 +120,7 @@
 
     <header>
         <nav class="navbar navbar-expand-lg">
-            <a class="navbar-brand mr-5" href="#">
+            <a class="navbar-brand mr-5 sm-none" href="#">
                 <img src="<?= WEB_ROOT ?>imgs/index/logo.svg" alt="">
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -119,6 +128,13 @@
                     <img src="<?= WEB_ROOT ?>imgs/index/ic-menu.svg" alt="" style="width: 48px; height: 10px;">
                 </span>
             </button>
+            <a class="navbar-brand lg-none" href="#">
+                <img src="<?= WEB_ROOT ?>imgs/index/logo.svg" alt="">
+            </a>
+            <a href="#" class="header-icon shopping-cart nav-link mx-0 lg-none">
+                <img src="<?= WEB_ROOT ?>imgs/index/ic-shopping.svg" alt="" style="width:48px;">
+            </a>
+            
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
@@ -134,28 +150,30 @@
                         <a class="nav-link" href="#">新鮮事</a>
                     </li>
                 </ul>
-                <form class="header-search" method="POST" name="header-search" class="form-inline my-2 my-lg-0">
+                <form class="header-search mr-3" method="POST" name="header-search" class="form-inline ">
                     <input class="search" type="text" name="search">
                     <button class="search-icon" type="submit">
                         <img src="<?= WEB_ROOT ?>imgs/index/ic-search.svg" alt="">
                     </button>
                 </form>
-                <a href="#" class="header-icon shopping-cart">
-                    <img src="<?= WEB_ROOT ?>imgs/index/ic-shopping.svg" alt="">
-                </a>
-                <div class="dropdown">
-                    <img class="dropdown header-icon" src="<?= WEB_ROOT ?>imgs/index/ic-member.svg" alt="">
-                    <div class="dropdown-content">
-                        <a href="#">會員資料</a>
-                        <a href="#">票券管理</a>
-                        <a href="#">我的收藏</a>
-                        <a href="#">優惠券管理</a>
-                        <a href="#">訊息管理</a>
-                        <a href="#">聯繫客服</a>
-                        <a href="#">登入/登出</a>
+                <!-- TO DO: 判斷是否登入 -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link mx-0" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img src="<?= WEB_ROOT ?>imgs/index/ic-member2.svg" alt="" style="width:48px;">
+                    </a>
+                    <div class="dropdown-menu p-0" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">會員資料</a>
+                        <a class="dropdown-item" href="#">票券管理</a>
+                        <a class="dropdown-item" href="#">我的收藏</a>
+                        <a class="dropdown-item" href="#">優惠券管理</a>
+                        <a class="dropdown-item" href="#">訊息管理</a>
+                        <a class="dropdown-item" href="#">聯繫客服</a>
+                        <a class="dropdown-item" href="#">登入/登出</a>
                     </div>
-                    <!-- TO DO: 判斷是否登入 -->
-                </div>
+                </li>
+                <a href="#" class="header-icon shopping-cart nav-link mx-0 sm-none">
+                    <img src="<?= WEB_ROOT ?>imgs/index/ic-shopping.svg" alt="" style="width:48px;">
+                </a>
             </div>
         </nav>
     </header>
