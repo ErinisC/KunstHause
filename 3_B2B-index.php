3bc4a52ce0c7c551d33d8cc2f41bd3c207d33d8c
kai_309 <gcobc0692@gmail.com>
2020-11-17 19:47:02 +0800

    B2B start!

--- 3_B2B-index.php
+++ 3_B2B-index.php
@@ -4,15 +4,23 @@
 <?php include __DIR__ . '/1_parts/1_head.php'; ?>
 
 <!-- 引入自己的ＣＳＳ -->
-<link rel="stylesheet" href="./css/4_productList-filter.css">
+<link rel="stylesheet" href="./css/3_B2B-index.css">
 
 <!-- 引入navbar -->
 <?php include __DIR__ . '/1_parts/2_navbar.php'; ?>
 
-
-<div class="container">
-    <h5>Hi</h5>
-
+<div class="container-fluid b2bindex">
+    <div class="row col-12">
+        <div class="indexcard">
+            <div class="picture">
+                <img src="" alt="">
+            </div>
+            <p class="organizer">主辦單位名稱</p>
+        </div>
+        <div class="indexcard"></div>
+        <div class="indexcard"></div>
+        <div class="indexcard"></div>
+    </div>
 </div>
 
 <?php include __DIR__ . '/1_parts/3_script.php'; ?>
