57979a1d49289e646fb0b77d6ff04e647db273ba
Vezona <vezona5tv@gmail.com>
2020-11-17 16:49:58 +0800

    config

--- 1_parts/0_config.php
+++ 1_parts/0_config.php
@@ -6,19 +6,19 @@ if (!isset($_SESSION)) {
 }
 
 // 連資料庫
-// $db_host = 'localhost';
-// $db_name = 'proj59';
-// $db_user = 'root';
-// $db_pass = '';
+$db_host = 'localhost';
+$db_name = 'proj59';
+$db_user = 'root';
+$db_pass = '';
 
 // 這一行不可以有任何空格
-// $dsn = "mysql:host={$db_host};dbname={$db_name};charset=utf8";
+$dsn = "mysql:host={$db_host};dbname={$db_name};charset=utf8";
 
-// $pdo_options = [
-//     // PDO::
-//     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-//     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-//     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
-// ];
+$pdo_options = [
+    // PDO::
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+];
 
-// $pdo = new PDO($dsn, $db_user, $db_pass, $pdo_options);
+$pdo = new PDO($dsn, $db_user, $db_pass, $pdo_options);
