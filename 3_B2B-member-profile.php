ee46891ef08316a927e80d352d9270fd33f09a47
kai_309 <gcobc0692@gmail.com>
2020-11-18 22:41:31 +0800

    member profile add

---
+++ 3_B2B-member-profile.php
@@ -0,0 +1,41 @@
+<?php $title = 'B2B首頁'; ?>
+
+<?php include __DIR__ . '/1_parts/0_config.php'; ?>
+<?php include __DIR__ . '/1_parts/1_head.php'; ?>
+
+<!-- 引入自己的ＣＳＳ -->
+<link rel="stylesheet" href="./css/3_B2B-member-profile.css">
+
+<!-- 引入navbar -->
+<?php include __DIR__ . '/1_parts/2_navbar.php'; ?>
+
+<div class="background">
+    <div class="container">
+        <div class="row mamberbg col-12">
+            <div class="mambertext">
+                <div class="picture">
+                    <svg class="people" xmlns="http://www.w3.org/2000/svg" width="140" height="140" viewBox="0 0 140 140">
+                        <path id="Union_7" data-name="Union 7" d="M0,140V122.5c0-19.254,31.5-35,70-35s70,15.75,70,35V140ZM35,35A35,35,0,1,1,70,70,35,35,0,0,1,35,35Z" fill="#7fc4fd" />
+                    </svg>
+                </div>
+                <p class="organizer">主辦單位名稱</p>
+                <p class="introduction">簡介</p>
+                <div class="introduction-text col-6">
+                    <p>LMalaysian comedian and content creator based in London, UK. Creator of Uncle Roger and Rice To Meet You podcast.
+                        <br>
+                        <br>
+                        For all enquiries please contact Duncan Hayes at United Agents via Olivia Jones ojones@unitedagents.co.uk</p>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<?php include __DIR__ . '/1_parts/3_script.php'; ?>
+
+<!-- 引入自己的ＪＳ -->
+<script src=""></script>
+
+<?php include __DIR__ . '/1_parts/4_footer.php'; ?>
