ee3e3ec2272779cb921e22e7489b4859603cb9a0
MadChick3n <326kav@gmail.com>
2020-11-18 22:30:45 +0800

    create file

---
+++ 3_B2B-create-event.php
@@ -0,0 +1,29 @@
+<?php $title = 'B2B首頁'; ?>
+
+<?php include __DIR__ . '/1_parts/0_config.php'; ?>
+<?php include __DIR__ . '/1_parts/1_head.php'; ?>
+
+<!-- 引入自己的ＣＳＳ -->
+<link rel="stylesheet" href="./css/3_B2B-Create-Event.css">
+
+<!-- 引入navbar -->
+<?php include __DIR__ . '/1_parts/2_navbar.php'; ?>
+
+<div class="container-fluid b2bcreate">
+    <div class="space mt-auto" style="height: 200px;"></div>
+    <div class="row col-12">
+        <h1 class="title">上架活動資料</h1>
+        <h2 class="sm-title">KunstHaus 使用者將透過下列資訊了解活動</h2>
+    </div>
+    <div class="space" style="height: 100px;"></div>
+    <span>活動Banner</span>
+    <div class="input"><input type="file" src="" alt=""></div>
+
+</div>
+
+<?php include __DIR__ . '/1_parts/3_script.php'; ?>
+
+<!-- 引入自己的ＪＳ -->
+<script src=""></script>
+
+<?php include __DIR__ . '/1_parts/4_footer.php'; ?>
