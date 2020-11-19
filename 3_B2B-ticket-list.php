645775f9e8fb4493643e9577c4cd00513356a312
kai_309 <gcobc0692@gmail.com>
2020-11-19 19:37:02 +0800

    ticket list

--- 3_B2B-ticket-list.php
+++ 3_B2B-ticket-list.php
@@ -21,8 +21,116 @@
                     <button class="modify2 btn btn-primary">未付款</button>
                     <button class="modify2 btn btn-primary">已取消</button>
                 </div>
+                <form class="header-search col-6" method="POST" name="header-search" class="form-inline ">
+                    <input class="search" type="text" name="search" placeholder="搜索訂單編號或活動名稱">
+                    <button class="search-icon" type="submit">
+                        <img src="<?= WEB_ROOT ?>imgs/index/ic-search.svg" alt="">
+                    </button>
+                </form>
 
+                <table>
+                    <thead>
+                        <tr class="tr d-flex mr-5">
+                            <td>訂單編號</td>
+                            <td>訂單日期</td>
+                            <td>合計</td>
+                            <td class="d-flex">訂單狀態<div class="white"></div>
+                            </td>
+                            <td class="d-flex">顯示
+                                <div class="white"></div>
+                            </td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="tr2 d-flex">
+                            <td>XDXD48763</td>
+                            <td>2020-10-26</td>
+                            <td>NT$590</td>
+                            <td>已付款</td>
+                            <td class="d-flex">
+                                <div class="modbutton text-center">
+                                    <button class="modify3 btn btn-primary">查閱</button>
+                                </div>
+                                <div class="modbutton text-center">
+                                    <button class="modify4 btn btn-primary">刪除訂單</button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr class="tr2 d-flex">
+                            <td>XDXD48763</td>
+                            <td>2020-10-26</td>
+                            <td>NT$590</td>
+                            <td>已付款</td>
+                            <td class="d-flex">
+                                <div class="modbutton text-center">
+                                    <button class="modify3 btn btn-primary">查閱</button>
+                                </div>
+                                <div class="modbutton text-center">
+                                    <button class="modify4 btn btn-primary">刪除訂單</button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr class="tr2 d-flex">
+                            <td>XDXD48763</td>
+                            <td>2020-10-26</td>
+                            <td>NT$590</td>
+                            <td>已付款</td>
+                            <td class="d-flex">
+                                <div class="modbutton text-center">
+                                    <button class="modify3 btn btn-primary">查閱</button>
+                                </div>
+                                <div class="modbutton text-center">
+                                    <button class="modify4 btn btn-primary">刪除訂單</button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr class="tr2 d-flex">
+                            <td>XDXD48763</td>
+                            <td>2020-10-26</td>
+                            <td>NT$590</td>
+                            <td>已付款</td>
+                            <td class="d-flex">
+                                <div class="modbutton text-center">
+                                    <button class="modify3 btn btn-primary">查閱</button>
+                                </div>
+                                <div class="modbutton text-center">
+                                    <button class="modify4 btn btn-primary">刪除訂單</button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr class="tr2 d-flex">
+                            <td>XDXD48763</td>
+                            <td>2020-10-26</td>
+                            <td>NT$590</td>
+                            <td>已付款</td>
+                            <td class="d-flex">
+                                <div class="modbutton text-center">
+                                    <button class="modify3 btn btn-primary">查閱</button>
+                                </div>
+                                <div class="modbutton text-center">
+                                    <button class="modify4 btn btn-primary">刪除訂單</button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr class="tr2 d-flex">
+                            <td>XDXD48763</td>
+                            <td>2020-10-26</td>
+                            <td>NT$590</td>
+                            <td>已付款</td>
+                            <td class="d-flex">
+                                <div class="modbutton text-center">
+                                    <button class="modify3 btn btn-primary">查閱</button>
+                                </div>
+                                <div class="modbutton text-center">
+                                    <button class="modify4 btn btn-primary">刪除訂單</button>
+                                </div>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
             </div>
+
         </div>
     </div>
 </div>
