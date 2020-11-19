db667ea2f9a3ccbe22826fc906f534265ef6c9cd
kai_309 <gcobc0692@gmail.com>
2020-11-19 11:38:18 +0800

    b2b

--- 3_B2B-member-profile.php
+++ 3_B2B-member-profile.php
@@ -13,6 +13,7 @@
     <div class="container">
         <div class="row mamberbg col-12">
             <div class="mambertext">
+                <img class="clip" src="/KunstHause//imgs/member/clip.svg" alt="">
                 <div class="picture">
                     <svg class="people" xmlns="http://www.w3.org/2000/svg" width="140" height="140" viewBox="0 0 140 140">
                         <path id="Union_7" data-name="Union 7" d="M0,140V122.5c0-19.254,31.5-35,70-35s70,15.75,70,35V140ZM35,35A35,35,0,1,1,70,70,35,35,0,0,1,35,35Z" fill="#7fc4fd" />
@@ -26,7 +27,61 @@
                         <br>
                         For all enquiries please contact Duncan Hayes at United Agents via Olivia Jones ojones@unitedagents.co.uk</p>
                 </div>
+                <div class="time d-flex col-6">
+                    <svg class="eye" xmlns="http://www.w3.org/2000/svg" width="39" height="21" viewBox="0 0 39 21">
+                        <g id="Eyes-icon" transform="translate(0 0)">
+                            <g id="eye-l" transform="translate(0 0)" fill="#fff" stroke="#000" stroke-width="3">
+                                <ellipse cx="8" cy="10.5" rx="8" ry="10.5" stroke="none" />
+                                <ellipse cx="8" cy="10.5" rx="6.5" ry="9" fill="none" />
+                            </g>
+                            <g id="eye-r" transform="translate(23 0)" fill="#fff" stroke="#000" stroke-width="3">
+                                <ellipse cx="8" cy="10.5" rx="8" ry="10.5" stroke="none" />
+                                <ellipse cx="8" cy="10.5" rx="6.5" ry="9" fill="none" />
+                            </g>
+                            <path id="Path_1307" data-name="Path 1307" d="M4.385,0A4.365,4.365,0,0,0,2.2.584L3.6,3.72.247,2.931A4.385,4.385,0,1,0,4.385,0Z" transform="translate(8.43 6.419) rotate(30)" />
+                            <path id="Path_1407" data-name="Path 1407" d="M4.385,0A4.365,4.365,0,0,0,2.2.584L3.6,3.72.247,2.931A4.385,4.385,0,1,0,4.385,0Z" transform="translate(31.186 6.419) rotate(30)" />
+                        </g>
+                    </svg>
+                    <div class="date">建立於2020-11-15</div>
+                </div>
+                <div class="history col-4">
+                    <div class="always">經常舉辦</div>
+                    <div class="type d-flex">
+                        <p>娛樂</p>
+                        <p>表演</p>
+                        <p>藝術展</p>
+                    </div>
+                </div>
+                <div class="contact d-flex col-8">
+                    <div class="phone d-flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="52.026" height="52.026" viewBox="0 0 52.026 52.026">
+                            <g id="Group_882" data-name="Group 882" transform="translate(-248.271 -113.701)">
+                                <circle id="Ellipse_340" data-name="Ellipse 340" cx="24.513" cy="24.513" r="24.513" transform="translate(249.771 115.201)" fill="#168fa4" stroke="#000" stroke-width="3" />
+                                <path id="Path_1490" data-name="Path 1490" d="M280.351,123.33H268.358a4.141,4.141,0,0,0-4.141,4.141v23.987a4.141,4.141,0,0,0,4.141,4.14h11.993a4.141,4.141,0,0,0,4.141-4.14V127.471A4.141,4.141,0,0,0,280.351,123.33Zm-8.751,2.111h5.51v1.51H271.6Zm2.755,29.252a1.934,1.934,0,1,1,1.934-1.934A1.936,1.936,0,0,1,274.355,154.693Zm7.8-4.7h-15.6V128.689h15.6Zm-7.8,1.361a1.4,1.4,0,1,0,1.4,1.4A1.4,1.4,0,0,0,274.355,151.357Z" fill="#fff" />
+                            </g>
+                        </svg>
+                        <p class="number">0900-001-333</p>
+                    </div>
+                    <div class="e-mail d-flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="52.026" height="52.026" viewBox="0 0 52.026 52.026">
+                            <g id="Group_885" data-name="Group 885" transform="translate(-248.271 -407.343)">
+                                <g id="Group_883" data-name="Group 883">
+                                    <circle id="Ellipse_341" data-name="Ellipse 341" cx="24.513" cy="24.513" r="24.513" transform="translate(249.771 408.843)" fill="#168fa4" stroke="#000" stroke-width="3" />
+                                </g>
+                                <g id="Group_884" data-name="Group 884">
+                                    <path id="Path_1491" data-name="Path 1491" d="M281.178,435.343l7.687,6.145a2.12,2.12,0,0,0,.306-1.1V426.32a2.114,2.114,0,0,0-.456-1.313l-11.692,11.661a3.874,3.874,0,0,1-5.478,0l-11.7-11.647a2.12,2.12,0,0,0-.446,1.3v14.071a2.118,2.118,0,0,0,.332,1.138l7.659-6.184.856,1.061-7.422,5.992a2.132,2.132,0,0,0,.7.121h25.518a2.122,2.122,0,0,0,.754-.14l-7.47-5.971Z" fill="#fff" />
+                                    <path id="Path_1492" data-name="Path 1492" d="M274.284,436.439a2.5,2.5,0,0,0,1.776-.735l11.481-11.451a2.149,2.149,0,0,0-.5-.061H261.525a2.083,2.083,0,0,0-.515.066l11.5,11.444A2.5,2.5,0,0,0,274.284,436.439Z" fill="#fff" />
+                                </g>
+                            </g>
+                        </svg>
+                        <p class="number">kunsthaus@gmail.com</p>
+                    </div>
+                </div>
 
+            </div>
+
+            <div class="modbutton text-center">
+                <button class="modify btn btn-primary">返回會員頁面</button>
             </div>
 
         </div>
