3446a7dd8931658f2b50449dec1f06fbde6f86d1
Judy Kuo <juiming0506@gmail.com>
2020-11-22 22:08:50 +0800

    -a modal

--- 2_member-order.php
+++ 2_member-order.php
@@ -85,34 +85,63 @@
 
 
 <div class="container">
-        <div class="row justify-content-center">
-            <nav aria-label="Page navigation example">
-                <ul class="pagination">
-                    <!-- 前一頁 -->
-                    <li class="page-item">
-                        <a class="page-link" href="#"><i class="fas fa-arrow-left"></i></a>
-                    </li>
-                    <!-- 中間頁數 -->
-                    <li class="page-item active">
-                        <a class="page-link" href="#">1</a>
-                    </li>
-                    <li class="page-item">
-                        <a class="page-link" href="#">2</a>
-                    </li>
-                    <li class="page-item">
-                        <a class="page-link" href="#">3</a>
-                    </li>
-                    <!-- 後一頁 -->
-                    <li class="page-item">
-                        <a class="page-link" href="#">
-                            <i class="fas fa-arrow-right"></i>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
+    <div class="row justify-content-center">
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <!-- 前一頁 -->
+                <li class="page-item">
+                    <a class="page-link" href="#"><i class="fas fa-arrow-left"></i></a>
+                </li>
+                <!-- 中間頁數 -->
+                <li class="page-item active">
+                    <a class="page-link" href="#">1</a>
+                </li>
+                <li class="page-item">
+                    <a class="page-link" href="#">2</a>
+                </li>
+                <li class="page-item">
+                    <a class="page-link" href="#">3</a>
+                </li>
+                <!-- 後一頁 -->
+                <li class="page-item">
+                    <a class="page-link" href="#">
+                        <i class="fas fa-arrow-right"></i>
+                    </a>
+                </li>
+            </ul>
+        </nav>
 
+    </div>
+</div>
+
+<!-- Modal -->
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+    Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
         </div>
     </div>
+</div>
+
+
 
 
 <!-- JQ -->
