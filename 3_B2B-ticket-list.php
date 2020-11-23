670dccaff0ecf23daf23d247b3957f0b90dab812
kai_309 <gcobc0692@gmail.com>
2020-11-23 11:09:32 +0800

    b2b add

--- 3_B2B-ticket-list.php
+++ 3_B2B-ticket-list.php
@@ -52,7 +52,7 @@
                                     <button class="modify3 btn btn-primary">查閱</button>
                                 </div>
                                 <div class="modbutton text-center">
-                                    <button class="modify4 btn btn-primary">刪除訂單</button>
+                                    <button class="modify4 btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">刪除訂單</button>
                                 </div>
                             </td>
                         </tr>
@@ -66,7 +66,7 @@
                                     <button class="modify3 btn btn-primary">查閱</button>
                                 </div>
                                 <div class="modbutton text-center">
-                                    <button class="modify4 btn btn-primary">刪除訂單</button>
+                                    <button class="modify4 btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">刪除訂單</button>
                                 </div>
                             </td>
                         </tr>
@@ -80,7 +80,7 @@
                                     <button class="modify3 btn btn-primary">查閱</button>
                                 </div>
                                 <div class="modbutton text-center">
-                                    <button class="modify4 btn btn-primary">刪除訂單</button>
+                                    <button class="modify4 btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">刪除訂單</button>
                                 </div>
                             </td>
                         </tr>
@@ -94,7 +94,7 @@
                                     <button class="modify3 btn btn-primary">查閱</button>
                                 </div>
                                 <div class="modbutton text-center">
-                                    <button class="modify4 btn btn-primary">刪除訂單</button>
+                                    <button class="modify4 btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">刪除訂單</button>
                                 </div>
                             </td>
                         </tr>
@@ -108,7 +108,7 @@
                                     <button class="modify3 btn btn-primary">查閱</button>
                                 </div>
                                 <div class="modbutton text-center">
-                                    <button class="modify4 btn btn-primary">刪除訂單</button>
+                                    <button class="modify4 btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">刪除訂單</button>
                                 </div>
                             </td>
                         </tr>
@@ -122,12 +122,47 @@
                                     <button class="modify3 btn btn-primary">查閱</button>
                                 </div>
                                 <div class="modbutton text-center">
-                                    <button class="modify4 btn btn-primary">刪除訂單</button>
+                                    <button class="modify4 btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">刪除訂單</button>
                                 </div>
                             </td>
                         </tr>
-
                     </tbody>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered" role="document">
+                            <div class="modal-content pt-3 mx-auto">
+                                <div class="tap">
+
+                                </div>
+
+                                <div class="modal-header d-flex flex-column">
+                                    <div class="g-check mx-auto mt-3">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="140" height="140" viewBox="0 0 128 128">
+                                            <g id="Exclamation_mark" data-name="Exclamation mark" transform="translate(-895.422 -303.047)">
+                                                <g id="Ellipse_338" data-name="Ellipse 338" transform="translate(895.422 303.047)" fill="none" stroke="#ed5b4c" stroke-width="3">
+                                                    <circle cx="64" cy="64" r="64" stroke="none" />
+                                                    <circle cx="64" cy="64" r="62.5" fill="none" />
+                                                </g>
+                                                <path id="Path_1480" data-name="Path 1480" d="M-248.277,183.461h-1.9a6.86,6.86,0,0,1-6.836-6.274l-4.336-50.505a6.86,6.86,0,0,1,6.835-7.448h10.568a6.861,6.861,0,0,1,6.836,7.448l-4.336,50.505A6.861,6.861,0,0,1-248.277,183.461Z" transform="translate(1208.792 203.813)" fill="#ed5b4c" />
+                                                <circle id="Ellipse_339" data-name="Ellipse 339" cx="9" cy="9" r="9" transform="translate(951 393)" fill="#ed5b4c" />
+                                            </g>
+                                        </svg>
+                                    </div>
+                                    <div class="modal-title mx-auto mt-3" id="exampleModalCenterTitle">是否確認刪除訂單!
+                                    </div>
+                                </div>
+                                <div class="modal-footer mx-auto">
+                                    <button type="button" class="yes btn btn-secondary" data-dismiss="modal" style="background-color: #ED5B4C">是</button>
+                                </div>
+
+                                <div class="modal-footer mx-auto">
+                                    <button type="button" class="closebutton btn btn-secondary" data-dismiss="modal" style="background-color: #fff">關閉視窗</button>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
                 </table>
             </div>
 
