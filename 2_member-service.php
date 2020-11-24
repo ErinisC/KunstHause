aadc6484af405ec2339e52e870e4bdf5abf6b0a3
Judy Kuo <juiming0506@gmail.com>
2020-11-24 18:43:54 +0800

    -a animation

--- 2_member-service.php
+++ 2_member-service.php
@@ -17,7 +17,7 @@
                     <span class="label text-center">姓名</span>
                     <input name="your-name" class="inputbox" type="text" aria-required="true" maxlength="50" required>
                 </div>
-                <div class="title d-flex mb-4">
+                <div class="mail-title d-flex mb-4">
                     <span class="label text-center">主旨</span>
                     <input name="your-title" class="inputbox" type="text" aria-required="true" maxlength="50" required>
                 </div>
@@ -37,7 +37,39 @@
             </form>
 
         </div>
-        <div class="col-lg-4 sm-none"></div>
+        <div class="col-lg-1"></div>
+        <div class="col-lg-5 sm-none">
+            <div id="shapes-background">
+                <div id="shape-1" class="shake-1">
+                    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 395.12 390.45" style="enable-background:new 0 0 395.12 390.45;" xml:space="preserve">
+                        <path class="st0" d="M336.94,1.68c-57.33,10.3-114.67,20.54-172.02,30.73C106.05,42.66,74.41,71.46,60.78,115
+                        C35.16,192.61,10.98,265.57,0,346.77c3.45,70.24,79.99,43.14,107.46,6.4c72.93-99.09,169.56-180.26,256.03-266.98
+                        C423.42,24.09,388.87-7.92,336.94,1.68z" />
+                    </svg>
+                </div>
+                <div id="shape-2" class="shake-2">
+                    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 311.79 306.44" style="enable-background:new 0 0 311.79 306.44;" xml:space="preserve">
+                        <path class="st0" d="M302.5,91.13C252.04,61.54,200.9,33.31,149.33,5.7c-40.95-19.98-77.42,16.09-86.8,54
+                        C39.41,132.7,11.28,201.07,0.06,277.1c-1.58,26.53,28.82,40.33,54.98,18.61c69.02-58.29,148.07-104.63,224.64-152
+                        C311.13,124.3,320.99,101.77,302.5,91.13z" />
+                    </svg>
+                </div>
+                <div id="shape-3" class="shake-3">
+                    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 114.94 109.18" style="enable-background:new 0 0 114.94 109.18;" xml:space="preserve">
+                        <path class="st0" d="M96.12,4.25C91.29-3.82,79.23,0.48,73.41,9.8C53.62,37.66,29.51,61.5,6.94,87.05
+                        c-14.12,16.85-4.87,26.17,11.4,20.43c26.81-9.36,53.72-18.39,80.71-27.24c10.55-3.41,17.19-11.47,15.68-18.82
+                        C110.92,40.96,105.23,22.94,96.12,4.25z" />
+                    </svg>
+                </div>
+            </div>
+
+            <div id="title-shape">
+                <p class="title blur">KunstHaus</p>
+            </div>
+
+
+
+        </div>
     </div>
 </div>
 <div class="space"></div>
