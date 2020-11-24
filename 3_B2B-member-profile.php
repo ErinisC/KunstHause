e777b401d8b1fb56b8f9f3ae77dacf0eb4df5de9
kai_309 <gcobc0692@gmail.com>
2020-11-24 11:56:39 +0800

    member-profile rwd add

--- 3_B2B-member-profile.php
+++ 3_B2B-member-profile.php
@@ -21,7 +21,7 @@
                 </div>
                 <p class="organizer">主辦單位名稱</p>
                 <p class="introduction">簡介</p>
-                <div class="introduction-text col-6">
+                <div class="introduction-text col-10 col-xl-6">
                     <p>LMalaysian comedian and content creator based in London, UK. Creator of Uncle Roger and Rice To Meet You podcast.
                         <br>
                         <br>
@@ -44,7 +44,7 @@
                     </svg>
                     <div class="date">建立於2020-11-15</div>
                 </div>
-                <div class="history col-4">
+                <div class="history col-xl-4 col-10">
                     <div class="always">經常舉辦</div>
                     <div class="type d-flex">
                         <p>娛樂</p>
@@ -52,9 +52,9 @@
                         <p>藝術展</p>
                     </div>
                 </div>
-                <div class="contact d-flex col-8">
-                    <div class="phone d-flex">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="52.026" height="52.026" viewBox="0 0 52.026 52.026">
+                <div class="contact d-flex col-xl-4 col-12">
+                    <div class="phone d-flex col-12">
+                        <svg class="phoneimg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52.026 52.026">
                             <g id="Group_882" data-name="Group 882" transform="translate(-248.271 -113.701)">
                                 <circle id="Ellipse_340" data-name="Ellipse 340" cx="24.513" cy="24.513" r="24.513" transform="translate(249.771 115.201)" fill="#168fa4" stroke="#000" stroke-width="3" />
                                 <path id="Path_1490" data-name="Path 1490" d="M280.351,123.33H268.358a4.141,4.141,0,0,0-4.141,4.141v23.987a4.141,4.141,0,0,0,4.141,4.14h11.993a4.141,4.141,0,0,0,4.141-4.14V127.471A4.141,4.141,0,0,0,280.351,123.33Zm-8.751,2.111h5.51v1.51H271.6Zm2.755,29.252a1.934,1.934,0,1,1,1.934-1.934A1.936,1.936,0,0,1,274.355,154.693Zm7.8-4.7h-15.6V128.689h15.6Zm-7.8,1.361a1.4,1.4,0,1,0,1.4,1.4A1.4,1.4,0,0,0,274.355,151.357Z" fill="#fff" />
@@ -62,8 +62,8 @@
                         </svg>
                         <p class="number">0900-001-333</p>
                     </div>
-                    <div class="e-mail d-flex">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="52.026" height="52.026" viewBox="0 0 52.026 52.026">
+                    <div class="e-mail d-flex col-12">
+                        <svg class="mailimg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52.026 52.026">
                             <g id="Group_885" data-name="Group 885" transform="translate(-248.271 -407.343)">
                                 <g id="Group_883" data-name="Group 883">
                                     <circle id="Ellipse_341" data-name="Ellipse 341" cx="24.513" cy="24.513" r="24.513" transform="translate(249.771 408.843)" fill="#168fa4" stroke="#000" stroke-width="3" />
