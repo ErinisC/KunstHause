6123f038de2d02248b753b54b9213b48d2bcf772
Judy Kuo <juiming0506@gmail.com>
2020-11-24 16:52:33 +0800

    -m form

--- 2_member-service.php
+++ 2_member-service.php
@@ -11,13 +11,38 @@
 <div class="space"></div>
 <div class="container">
     <div class="row">
-        <div class="col-lg-5 form">
-            <div class="name d-flex">
-                <input class="inputbox" type="text" placeholder="請填寫主辦單位名稱"> 
-            </div>
+        <div class="col-lg-6 col-12">
+            <form class="service" action="POST" novalidate="novalidate">
+                <div class="name d-flex mb-4">
+                    <span class="label text-center">姓名</span>
+                    <input name="your-name" class="inputbox" type="text" aria-required="true" maxlength="50" required>
+                </div>
+                <div class="title d-flex mb-4">
+                    <span class="label text-center">主旨</span>
+                    <input name="your-title" class="inputbox" type="text" aria-required="true" maxlength="50" required>
+                </div>
+                <div class="email-ad d-flex mb-4">
+                    <span class="label text-center">EMAIL</span>
+                    <input name="your-email" class="inputbox" type="email" aria-required="true" maxlength="50" required>
+                </div>
+                <div class="content mb-4">
+                    <p class="label text-center short-label">內容</p>
+                    <textarea name="your-content" class="inputbox w-100" type="text" aria-required="true" rows="8" required>
+                    </textarea>
+                </div>
+                <div class="buttons">
+                    <button type="submit" class="btn service-submit col-4 mr-2">寄送信件</button>
+                    <button type="reset" class="btn clear col-4">清除內容</button>
+                </div>
+            </form>
+
         </div>
+        <div class="col-lg-4 sm-none"></div>
     </div>
 </div>
+<div class="space"></div>
+
+
 
 
 <!-- JQ -->
@@ -25,11 +50,5 @@
 
 <!-- Boostrap JS -->
 <script src="./bootstrap/js/bootstrap.bundle.js"></script>
-
-<script>
-    // const dallorCommas = function(n) {
-    //     return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-    // };
-</script>
 
 <?php include __DIR__ . '/1_parts/4_footer.php'; ?>
