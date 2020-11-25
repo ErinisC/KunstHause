a5c79e56437cda17e8420fa3bcb465666dc58df5
kai_309 <gcobc0692@gmail.com>
2020-11-25 13:35:38 +0800

    sign in add

--- 3_B2B-sign-in.php
+++ 3_B2B-sign-in.php
@@ -119,7 +119,33 @@
             <div class="modbutton text-center col-xl-8 col-12">
                 <div class="okbutton col-xl-6 col-10 d-flex">
                     <button class="modify1 col-5 btn btn-primary">取消註冊</button>
-                    <button class="modify2 col-5 btn btn-primary" disabled>完成註冊</button>
+                    <button class="modify2 col-5 btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">完成註冊</button>
+                </div>
+            </div>
+
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content pt-3 mx-auto">
+                        <div class="tap">
+
+                        </div>
+
+                        <div class="modal-header d-flex flex-column">
+                            <div class="g-check mx-auto mt-5">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="148.234" height="106.909" viewBox="0 0 148.234 106.909">
+                                    <path id="Path_644" data-name="Path 644" d="M-97.709,57.527a10.375,10.375,0,0,0-14.63,0l-78.928,78.928-34.011-34.01a10.375,10.375,0,0,0-14.63,0,10.377,10.377,0,0,0,0,14.631l41.325,41.324a10.375,10.375,0,0,0,14.63,0h0l86.242-86.244A10.375,10.375,0,0,0-97.709,57.527Z" transform="translate(242.925 -54.509)" fill="#168fa4" />
+                                </svg>
+                            </div>
+                            <div class="modal-title mx-auto mt-4" id="exampleModalCenterTitle">修改完成
+                            </div>
+                        </div>
+
+                        <div class="modal-footer mx-auto mt-2">
+                            <button type="button" class="closebutton btn btn-secondary" data-dismiss="modal" style="background-color: #fff">關閉視窗</button>
+                        </div>
+
+                    </div>
                 </div>
             </div>
 
