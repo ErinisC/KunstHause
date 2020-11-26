27fffe50c7bd68be76b288d061905d4ced82d796
Judy Kuo <juiming0506@gmail.com>
2020-11-26 09:15:52 +0800

    -m slider

--- 0_index.php
+++ 0_index.php
@@ -30,30 +30,41 @@
 
 <?php include __DIR__ . '/1_parts/2_navbar-lg.php'; ?>
 
+
+
+
 <section class="main grid-blue pb-5">
     <div class="container-fluid main-activities p-0">
-        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <a href="#" class="">
-                        <img class="d-block w-100 mainact-1" src="<?= WEB_ROOT ?>imgs/event/event-lg/TPE-45.png.jpg" alt="First slide">
-                    </a>
+                    <img class="d-block w-100 mainact-1" src="<?= WEB_ROOT ?>imgs/event/event-lg/TPE-59.jpg.jpg" alt="First slide">
                 </div>
                 <div class="carousel-item">
-                    <a href="#" class="">
-                        <img class="d-block w-100 mainact-1" src="<?= WEB_ROOT ?>imgs/event/event-lg/TPE-46.png.jpg" alt="Second slide">
-                    </a>
+                    <img class="d-block w-100 mainact-1" src="<?= WEB_ROOT ?>imgs/event/event-lg/TPE-60.jpg.jpg" alt="First slide">
                 </div>
                 <div class="carousel-item">
-                    <a href="#" class="">
-                        <img class="d-block w-100 mainact-1" src="<?= WEB_ROOT ?>imgs/event/event-lg/TPE-47.png.jpg" alt="Third slide">
-                    </a>
+                    <img class="d-block w-100 mainact-1" src="<?= WEB_ROOT ?>imgs/event/event-lg/TXG-02.jpeg.png.jpg" alt="First slide">
                 </div>
             </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
-        <p class="section-title-l w-100 text-center cw m-100">活動列表</p>
     </div>
+
     <div class="container-fluid w-85 pb-5">
+        <p class="section-title-l w-100 text-center cw m-100">活動列表</p>
         <div class="pickup-act row">
             <div class="col-lg-4 col-md-12 col-sm-12 col-12 px-4">
                 <div class="card">
