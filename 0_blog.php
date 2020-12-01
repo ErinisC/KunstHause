dae387a686b4bec9ad93c5524f12ca635b6c8bfe
Judy Kuo <juiming0506@gmail.com>
2020-12-01 16:35:01 +0800

    -a blog page

---
+++ 0_blog.php
@@ -0,0 +1,26 @@
+<?php include __DIR__ . '/1_parts/0_config.php'; ?>
+
+<?php $title = 'KunstHaus | 新鮮事'; ?>
+<?php include __DIR__ . '/1_parts/1_head.php'; ?>
+<?php include __DIR__ . '/1_parts/2_navbar.php'; ?>
+
+<!--引入blog的css-->
+<link rel="stylesheet" href="./css/2_member-order.css">
+
+
+
+
+
+
+
+<!-- JQ -->
+<script src="./libary/jquery-3.5.1.js"></script>
+
+<!-- Boostrap JS -->
+<script src="./bootstrap/js/bootstrap.bundle.js"></script>
+
+<script>
+    
+</script>
+
+<?php include __DIR__ . '/1_parts/4_footer.php'; ?>
