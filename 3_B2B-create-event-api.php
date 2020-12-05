b2617ec9d8f700d51417de82d57a45bc8d406a21
MadChick3n <326kav@gmail.com>
2020-12-05 20:17:37 +0800

    -m PHP linked to Backend
    updated: <select>
                    rwd changes

--- 3_B2B-create-event-api.php
+++ 3_B2B-create-event-api.php
@@ -8,9 +8,9 @@ $output = [
     'postData' => $_POST,
 ];
 
-if (empty($_POST['event_name']) or empty($_POST['event_info']) or empty($_POST['start-datetime']) or empty($_POST['price'])) {
+if (empty($_POST['event_name']) or empty($_POST['event_info']) or empty($_POST['start-datetime']) or empty($_POST['end-datetime']) or empty($_POST['price'])) {
     $output['postData'] = $_POST;
-    $s_sql = "SELECT 1 FROM `kunsthaus` WHERE `products`";
+    $s_sql = "SELECT * FROM `kunsthaus` WHERE `products`";
     $s_stmt->execute([$_POST['event_name']]);
     if ($s_stmt->rowCount() == 1) {
         $output['info'] = '此活動名稱已被註冊';
@@ -22,13 +22,13 @@ if (empty($_POST['event_name']) or empty($_POST['event_info']) or empty($_POST['
 //TODO: 檢查資料格式
 
 $sql = "INSERT INTO `products`
-(`event_name`, `hastag`, `picture`,
- `categories`, `location`, `start-datetime`, `end-datetime`, 
+(`event_name`, 
+ `categories`, `location`,  
  `price`, `address`, `event_info`, `notice`, 
  `transportation`)
  VALUES (
-        ?, ?, ?,
-        ?, ?, ?, ?,
+        ?, 
+        ?, ?, 
         ?, ?, ?, ?,
         ? 
     )";
@@ -36,12 +36,8 @@ $sql = "INSERT INTO `products`
 $stmt = $pdo->prepare($sql);
 $stmt->execute([
     $_POST['event_name'],
-    $_POST['hastag'],
-    $_POST['picture'],
     $_POST['categories'],
     $_POST['location'],
-    $_POST['start-datetime'],
-    $_POST['end-datetime'],
     $_POST['price'],
     $_POST['address'],
     $_POST['event_info'],
